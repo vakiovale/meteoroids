c42429e1a2052e056fba0d9786d823fe00b2bb08
Valtteri Pyyhtiä <valtteri.pyyhtia@helsinki.fi>
2015-01-26 00:42:26 +0200

    added skeleton for Ship and Asteroid classes

---
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/gameobjects/physicobjects/Asteroid.java
@@ -0,0 +1,9 @@
+package meteoroids.Meteoroids.gameobjects.physicobjects;
+
+public class Asteroid extends PhysicsObject {
+
+	public Asteroid() {
+		super(5.0, 5.0, 50.0);
+	}
+	
+}
---
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/gameobjects/physicobjects/Ship.java
@@ -0,0 +1,9 @@
+package meteoroids.Meteoroids.gameobjects.physicobjects;
+
+public class Ship extends PhysicsObject {
+
+	public Ship() {
+		super(10.0);
+	}
+	
+}
