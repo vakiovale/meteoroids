ff215b81dd78e8ce1987e783ae6568d7161e888d
Valtteri Pyyhtiä <valtteri.pyyhtia@helsinki.fi>
2015-01-25 20:00:29 +0200

    removed unused imports

--- Meteoroids/src/main/java/meteoroids/Meteoroids/Main.java
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/Main.java
@@ -1,9 +1,5 @@
 package meteoroids.Meteoroids;
 
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-
 /**
  * Main application - Meteoroids
  *
