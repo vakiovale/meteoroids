3d6068f79cb6efb2eb093303d133b206d26da265
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-01-26 16:25:25 +0200

    added simple physics to PhysicsObject

--- Meteoroids/src/main/java/meteoroids/Meteoroids/gameobjects/physicsobjects/PhysicsObject.java
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/gameobjects/physicsobjects/PhysicsObject.java
@@ -20,7 +20,11 @@ public abstract class PhysicsObject extends DUGameObject implements IPosition {
 	protected Vector2d acceleration;
 	protected Vector2d velocity;
 	
+	// TODO: Instead of the actual mass, it could be wiser to use an inverse of mass.
+	// The mass itself is rarely used and it's inverse mass is more common.
+	// One way could be to add also an inverse mass variable;
 	protected double mass;
+	private double inverseMass;
 	
 	public PhysicsObject() {
 		this(new Vector2d(0.0, 0.0), new Vector2d(0.0, 0.0), new Vector2d(0.0, 0.0), new Vector2d(0.0, 0.0), 1.0);
@@ -32,7 +36,7 @@ public abstract class PhysicsObject extends DUGameObject implements IPosition {
 	    this.acceleration = acceleration;
 	    this.velocity = velocity;
 	    
-	    this.mass = mass <= 1.0 ? 1.0 : mass;
+	    this.mass = mass <= 1.0 ? 1.0 : mass; // Don't allow mass lower than 1.0
 	}
 	
 	public PhysicsObject(double posX, double posY) {
@@ -49,7 +53,18 @@ public abstract class PhysicsObject extends DUGameObject implements IPosition {
 	
 	@Override
 	public void update(double deltaTime) {
-	    //acceleration = sumOfForces / mass;
+	    sumOfForces.scale(inverseMass);
+	    acceleration = sumOfForces;
+	    
+	    Vector2d velocityAdd = (Vector2d)acceleration.clone();
+	    velocityAdd.scale(deltaTime);
+	    
+	    velocity.add(velocityAdd);
+	    
+	    Vector2d positionAdd = (Vector2d)velocity.clone();
+	    positionAdd.scale(deltaTime);
+	    
+	    position.add(positionAdd);
 	}
 	
 	/**
