1103f702f1d4a96c4936456b057b44dfbff85477
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2015-02-05 14:27:51 +0200

    added Munro font

---
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/controllers/resources/Text.java
@@ -0,0 +1,43 @@
+package meteoroids.Meteoroids.controllers.resources;
+
+import meteoroids.Meteoroids.Game;
+
+/**
+ * Create text.
+ * 
+ * @author vpyyhtia
+ *
+ */
+public class Text {
+
+    private String text;
+    private float x;
+    private float y;
+    
+    public Text(String text) {
+        this(text, Game.WIDTH/2-(Game.WIDTH/14), Game.HEIGHT/2);
+    }
+    
+    public Text(String text, float x, float y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public String getText() {
+        return text;
+    }
+    
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public float getX() {
+        return x;
+    }
+    
+    public float getY() {
+        return y;
+    }
+}
---
+++ Meteoroids/src/main/java/meteoroids/Meteoroids/controllers/resources/TextHandler.java
@@ -0,0 +1,78 @@
+package meteoroids.Meteoroids.controllers.resources;
+
+import java.awt.Font;
+import java.io.InputStream;
+import java.util.ArrayDeque;
+
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.util.ResourceLoader;
+
+import meteoroids.Meteoroids.Game;
+import meteoroids.Meteoroids.gameobjects.Drawable;
+
+/**
+ * Handles drawing texts to the screen.
+ * 
+ * @author vpyyhtia
+ *
+ */
+public class TextHandler implements Drawable {
+
+    private ArrayDeque<Text> texts;
+
+    private TrueTypeFont font;
+    
+    public TextHandler() {
+        texts = new ArrayDeque<>();
+        initFont();
+    }
+    
+    private void initFont() {
+        try {
+            InputStream inputStream = ResourceLoader.getResourceAsStream("Munro.ttf");
+            
+            Font awtFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+            awtFont = awtFont.deriveFont(30f*2);
+            font = new TrueTypeFont(awtFont, false);
+     
+        } catch (Exception e) {
+            // TODO: Error message
+            e.printStackTrace();
+            System.exit(0);
+        }        
+    }
+    
+    public void addText(String text) {
+        texts.push(new Text(text));
+    }
+    
+    public String removeText() {
+        if(!texts.isEmpty()) {
+            return texts.pop().getText();
+        }
+        return null;
+    }
+
+    @Override
+    public void draw() {
+        if(!texts.isEmpty()) {
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glMatrixMode(GL11.GL_PROJECTION);
+            GL11.glLoadIdentity();
+            GL11.glOrtho(0, Game.WIDTH, Game.HEIGHT, 0, 1, -1);
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+            for(Text text : texts) {
+                font.drawString(text.getX(), text.getY(), text.toString(), Color.lightGray);
+            }
+        }
+        
+    }
+
+    @Override
+    public int getID() {
+        return -1;
+    }
+
+}
